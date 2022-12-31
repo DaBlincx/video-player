17e3b6378f0487158c6dedc2fd43eacf875f24d9
DaBlincx <blincxyt@gmail.com>
2022-12-31 23:40:18 +0100

    adding volume control, works basically

--- assets/script.js
+++ assets/script.js
@@ -3,46 +3,28 @@ var playPauseBtn = document.getElementById("play-pause");
 var progress = document.getElementById("progress");
 var progressbg = document.getElementById("progress-bg");
 var progresshover = document.getElementById("progress-hover");
+var volumebar = document.getElementById("volume-bar");
 
 function togglePlayPause() {
     if (video.paused || video.ended) {
         playPauseBtn.className = "fa-solid fa-pause";
         video.play();
-        console.log("play");
     } else {
         playPauseBtn.className = "fa-solid fa-play";
         video.pause();
-        console.log("pause");
     }
 }
 
 function updateProgress(progpercent) {
     progress.style.width = progpercent * 100 + "%";
-    console.log("updated to " + progpercent);
 }
 
 function clickUpdate(event) {
     var pos = event.offsetX;
-    console.log(pos);
-
     var pospercent = pos / progressbg.offsetWidth;
-
-    console.log(progressbg.offsetWidth);
-    console.log("clicked at " + pospercent);
-
     updateProgress(pospercent);
-    console.log(video.currentTime);
-
     newtime = pospercent * video.duration;
-
-    console.log(newtime);
-
     video.currentTime = newtime;
-    
-    
-    console.log(video.duration);
-    console.log(pospercent)
-    console.log(video.currentTime);
 }
 
 function updateHoverBar(event) {
@@ -56,17 +38,19 @@ function rmHoverBar() {
 }
 
 progressbg.addEventListener("click", function (event) {
-    console.log("clicked");
     clickUpdate(event);
 }, false);
 
 video.addEventListener("timeupdate", function () {
     var timepercent = video.currentTime / video.duration;
-    console.log(video.currentTime, video.duration, timepercent);
     updateProgress(timepercent);
     if (video.ended || video.paused) {
         playPauseBtn.className = "fa-solid fa-play";
     } else {
         playPauseBtn.className = "fa-solid fa-pause";
     }
+}, false);
+
+volumebar.addEventListener("input", function () {
+    video.volume = volumebar.value;
 }, false);
