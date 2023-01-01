1957a40ede8fdf7922fb38a54d8f2351390a1c5b
DaBlincx <blincxyt@gmail.com>
2023-01-01 15:05:06 +0100

    HAHAHAH WORKING VOLUME SLIDER

--- assets/script.js
+++ assets/script.js
@@ -6,6 +6,7 @@ var progresshover = document.getElementById("progress-hover");
 var videoDurationDisplay = document.getElementById("time-video-duration-display");
 var videoCurrentTimeDisplay = document.getElementById("time-video-current-time-display");
 var volumebar = document.getElementById("volume-bar");
+var volumebutton = document.getElementById("volume-button")
 
 function checkURLParams() {
     const queryString = window.location.search;
@@ -51,20 +52,6 @@ function rmHoverBar() {
     progresshover.style.width = 0;
 }
 
-progressbg.addEventListener("click", function (event) {
-    clickUpdate(event);
-}, false);
-
-video.addEventListener("timeupdate", function () {
-    updateProgress(video.currentTime / video.duration);
-    if (video.ended || video.paused) { playPauseBtn.className = "fa-solid fa-play"; }
-    else {playPauseBtn.className = "fa-solid fa-pause";}
-}, false);
-
-video.addEventListener("click", function () {
-    togglePlayPause()
-})
-
 function formTimeString(seconds) {
     seconds = Math.floor(seconds)
     var hours = 0;
@@ -85,17 +72,67 @@ function formTimeString(seconds) {
     }
 }
 
+function toggleMute() {
+
+    if (video.muted) {
+        video.muted = false;
+        volumebutton.className = getVolClasses(video.volume);
+    } else {
+        video.muted = true;
+        volumebutton.className = "fa-solid fa-volume-xmark";
+    }
+}
+
+function getVolClasses(volume) {
+    if (volume == 0) {
+        return "fa-solid fa-volume-xmark"
+    } else if (0 < volume && volume <= 0.33) {
+        return "fa-solid fa-volume-off"
+    } else if (0.33 < volume && volume <= 0.66) {
+        return "fa-solid fa-volume-low"
+    } else if (0.66 < volume && volume <= 1) {
+        return "fa-solid fa-volume-high"
+    } else {
+        return "fa-solid fa-volume-high"
+    }
+}
+
+
+/*
+ill implement this later lol
+
 function checkBufferSize(element) {
     for (let i = 0; i < element.buffered.length; i++) {
         const startX = element.buffered.start(i) * inc;
         const endX = element.buffered.end(i) * inc;
         const width = endX - startX;
     }
-}
+}*/
+
+progressbg.addEventListener("click", function (event) {
+    clickUpdate(event);
+}, false);
+
+video.addEventListener("timeupdate", function () {
+    updateProgress(video.currentTime / video.duration);
+    if (video.ended || video.paused) {
+        playPauseBtn.className = "fa-solid fa-play";
+    } else {
+        playPauseBtn.className = "fa-solid fa-pause";
+    }
+    
+}, false);
+
+video.addEventListener("click", function () {
+    togglePlayPause()
+})
 
 setTimeout(() => videoDurationDisplay.innerText = formTimeString(video.duration), 180)
 
 
 volumebar.addEventListener("input", function () {
     video.volume = volumebar.value;
+    if (!video.muted) {
+        volumebutton.className = getVolClasses(video.volume);
+    }
 }, false);
